67365ea2d16ec9a05fc8a671a1b90fb799628249
it367 <it@kidshinehawaii.com>
2026-03-12 02:44:29 +0800

    Change favicon link to use PNG format

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,7 @@ export default function RootLayout({ children }) {
   return (
 <html lang="en">
         <head>
-          <link rel="icon" href="/favicon.ico" sizes="any" />
+          <link rel="icon" type="image/png" href="/favicon.png" />
         </head>
         <body>{children}</body>
       </html>
